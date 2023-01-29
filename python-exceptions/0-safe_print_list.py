1cb9ad93c859b02d3dce17b315eba3cb0557001e
victorfabian22 <fabianromeroa22@gmail.com>
2023-01-29 15:54:05 -0800

    Safe print

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for i in range(X):
+            print("{}".format(my_list[i], end="")
+
+    except (IndexError, TypeError):
+        print()
+        return i
+    print()
+    retunr (X)
