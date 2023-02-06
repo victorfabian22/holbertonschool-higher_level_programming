366d5d3fe481e12ce41574df0fb37c8c75638ddd
victorfabian22 <fabianromeroa22@gmail.com>
2023-02-06 15:23:03 -0800

    Square 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class Square"""
+
+
+class Square:
+    """Defines a square
+    """
+    def __init__(self, size):
+        """Initializacion of objects
+        Args:
+            size(int): size of the square.
+        """
+        self.__size = size
