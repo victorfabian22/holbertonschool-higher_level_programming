370b78569c4750d03da06ecb63a23052b8beeb12
victorfabian22 <fabianromeroa22@gmail.com>
2023-02-12 15:02:35 -0800

    inheritance

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -13,4 +13,3 @@ def inherits_from(obj, a_class):
     otherwise return False
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
-
---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Task 5 Class
+"""
+
+
+class BaseGeometry:
+    """
+    Note: empty.
+    """
+    pass
