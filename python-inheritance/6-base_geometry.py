81f117c989947a6196ed1c9ad17f0c9700ef9eda
victorfabian22 <fabianromeroa22@gmail.com>
2023-02-12 15:03:55 -0800

    base geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Task 6 Class based on previous task
+"""
+
+
+class BaseGeometry:
+    """
+    Public instance method
+    """
+
+    def area(self):
+        """
+        raises an exception
+        """
+        raise Exception("area() is not implemented")
