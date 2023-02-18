1508c873c172278e506f1035bca63cae197111c6
victorfabian22 <fabianromeroa22@gmail.com>
2023-02-18 08:03:00 -0800

    base

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,24 +1,45 @@
 #!/usr/bin/python3
-"""
-Class Module
-"""
+# Alex Nuñez <5694@holbertonstudents.com>
+"""Define a class Base."""
+import json
 
 
 class Base:
-    """
-    Initializing Class Base
-    """
+    """Create a class Base."""
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize a class."""
         if id is not None:
-            """ Assing the public instance attribute id"""
             self.id = id
         else:
-            """
-            Increment the private class
-            and assing the new value to the
-            public instance attribute id
-            """
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Return the JSON string representation of a list of dic."""
+        if list_dictionaries == [] or list_dictionaries is None:
+            return("[]")
+        else:
+            return(json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes the JSON string of list object to a file."""
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                f.write(Base.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string."""
+        if json_string is None or json_string == "[]":
+            return([])
+        else:
+            return(json.loads(json_string))
+
