1c8997525050b38e0a7cdb467d3dc5b84fac55fb
= <=>
2023-08-03 13:28:38 -0700

    SQL

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Script that creates the database 'hbtn_0d_usa' and 'states'
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+       id INT AUTO_INCREMENT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       PRIMARY KEY(id)
+);
