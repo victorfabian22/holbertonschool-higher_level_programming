c68adaa7a0ea3eeb1897579c66ab5e730c9f38c5
= <=>
2023-08-06 13:28:45 -0700

    Select States

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
-
 """Script that lists all 'states' from the database 'hbtn_0e_0_usa'"""
-
 
 import MySQLdb
 import sys
-
 
 def list_states(username, password, db_name):
     """Function to list a table
@@ -37,3 +34,4 @@ if __name__ == "__main__":
     password = sys.argv[2]
     db_name = sys.argv[3]
     list_states(username, password, db_name)
+    
