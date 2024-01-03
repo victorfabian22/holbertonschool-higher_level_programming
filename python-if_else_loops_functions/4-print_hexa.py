00b3cf823144471c806f84cb0a4ac90d1a7d01af
victorfabian22 <102700355+victorfabian22@users.noreply.github.com>
2024-01-03 14:23:24 -0500

    Delete python-if_else_loops_functions directory

--- python-if_else_loops_functions/4-print_hexa.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-for hexnum in range(0, 99):
-    print("{} = {}".format(hexnum, hex(hexnum)))
--- python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for lowstr in str:
-        if ord(lowstr) >= 97 and ord(lowstr) <= 122:
-            lowstr = chr(ord(lowstr) - 32)
-        print("{}".format(lowstr), end='')
-    else:
-        print("")
