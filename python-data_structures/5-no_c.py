e3ebabbb53ea7b3f1016c0e41d02eaff760f0ac1
victorfabian22 <102700355+victorfabian22@users.noreply.github.com>
2024-01-03 14:23:45 -0500

    Delete python-data_structures directory

--- python-data_structures/5-no_c.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    i = ""
-    for x in my_string:
-        if x != "c" and x != "C":
-            i += x
-    return (i)
--- python-data_structures/8-multiple_returns.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def multiple_returns(sentence):
-    a = len(sentence)
-    if a == 0:
-        return (0, "None")
-    else:
-        b = sentence[0]
-
-    return (a, b)
