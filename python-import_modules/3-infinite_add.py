cd0641ce0d677428245971e4e02e6c152f1c5e23
victorfabian22 <102700355+victorfabian22@users.noreply.github.com>
2024-01-03 14:23:34 -0500

    Delete python-import_modules directory

--- python-import_modules/3-infinite_add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    total = 0
-    for args in range(len(sys.argv) - 1):
-        total = total + int(sys.argv[args + 1])
-    print("{}".format(total))
--- python-import_modules/4-hidden_discovery.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-    hideContent = dir(hidden_4)
-    for hideNames in hideContent:
-        if hideNames[0:1] != "_":
-            print(hideNames)
